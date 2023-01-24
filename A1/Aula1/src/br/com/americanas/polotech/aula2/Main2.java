9d6f63a8de238b6d104fd94edc52f626c6d55707
ViniciusLimaDeOliveira <61565285+ViniciusLimaDeOliveira@users.noreply.github.com>
2023-01-23 21:40:34 -0300

    Adicionado Scanner

--- A1/Aula1/src/br/com/americanas/polotech/aula2/Main2.java
+++ A1/Aula1/src/br/com/americanas/polotech/aula2/Main2.java
@@ -1,4 +1,7 @@
 package br.com.americanas.polotech.aula2;
+
+import java.util.Scanner;
+
 /*
 você quer vender um notebook e está aceitando pagamento no cartão,
 porém existem algumas taxas da maquininha e
@@ -13,11 +16,26 @@ Na classe Main deverá imprimir:
 foi 'valorX' e você irá receber 'valorY'"
 */
 public class Main2 {
+
     public static void main(String[] args) {
-        Double valorNote = 1000.0;
-        valorComTaxa(valorNote,new AVistaCredito());
-        valorComTaxa(valorNote,new CreditoParcelado());
-        valorComTaxa(valorNote,new Debito());
+        Scanner scanner = new Scanner(System.in);
+        Double valorNote;
+        System.out.println("Qual valor da compra ?");
+        valorNote = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Qual metodo utizado? Debito - 1,Credito a vista - 2 , Credito Parcelado - 3");
+        int op = scanner.nextInt();
+        switch (op){
+            case 1: valorComTaxa(valorNote,new Debito());
+                break;
+            case 2: valorComTaxa(valorNote,new AVistaCredito());
+                break;
+            case 3: valorComTaxa(valorNote,new CreditoParcelado());
+                break;
+        }
+
+
+
     }
     public static void valorComTaxa(Double valorCompra, TaxaMaquininha metodo){
         System.out.println("Valor cobrado pela venda do Notebook no "+ metodo.getClass().getSimpleName()
